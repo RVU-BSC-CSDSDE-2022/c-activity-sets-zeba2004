14e12c17a7383b069683256089992cc75ab40ea0
zeba2004 <117371677+zeba2004@users.noreply.github.com>
2022-11-29 14:36:04 +0530

    Update problem03.c

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
 int main(){
- int a,b,sum;
+ int a,b;
  a=input();
  b=input();
- sum=add(a,b);
+ add(a, b);
  output(a,b,sum);
   } 
 
 int input(){
-  int a;
-  scanf("%d",&a);
-  return a;
+  int n;
+  scanf("%d",&n;
+  return n;
   }
 
 int add(int a,int b){
- int sum=a+b;
-  return sum; 
+
 }
 
-void output(int a,int b,int sum){
+void output(int a,int b,int sum}
+ sum =a+b;
  printf("the sum of %d and %d is %d",a,b,sum); 
 }
